80624975fc3f52ca1a33d4ed15ae025179dfc24b
Korey Conway <korey.conway@gmail.com>
2013-10-01 21:08:35 -0400

    - setup Xcode project - some project problems still to resolve - started adding comments and skeleton for monitor

---
+++ Assignment 1/controller/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  controller
+//
+//  Created by Korey Conway on 2013-10-01.
+//  Copyright (c) 2013 SG•Nexus. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
---
+++ Assignment 1/monitor/main.c
@@ -0,0 +1,42 @@
+//
+//  main.c
+//  monitor
+//
+//  Created by Korey Conway on 2013-10-01.
+//  Copyright (c) 2013 SG•Nexus. All rights reserved.
+//
+
+#include <stdio.h>
+
+#define INTERVAL 2
+
+int child_pid;
+int child_done = 0; // set to 1 to tell child to stop
+
+struct fifo_data controller_data {
+	int heartbeat_rate,
+	int pid
+};
+
+int main(int argc, const char * argv[])
+{
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
+
+void start_child(fifo_data data) {
+	// fork here
+}
+
+void end_child() {
+	// if we are child
+		// close ourselves
+	// if we are parent
+		// send signal to child to close
+}
+
+void create_controller_fifo() {
+	
+}
+
