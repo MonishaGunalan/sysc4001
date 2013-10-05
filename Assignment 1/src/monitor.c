cef278d309faf350964404206471ce91010e2ebb
monisha <moni.sind@gmail.com>
2013-10-04 21:58:46 -0400

    - default to tab spacing for project - added seed to random number generation

--- Assignment 1/src/monitor.c
+++ Assignment 1/src/monitor.c
@@ -7,10 +7,13 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define INTERVAL 2
 #define MAX_HEARTBEAT 100
 #define MIN_HEARTBEAT 60
+#define RAND_SEED (unsigned) time(NULL)
 
 int child_pid;
 int child_done = 0; // set to 1 to tell child to stop
@@ -24,6 +27,11 @@ int main(int argc, const char * argv[])
 {
 	// insert code here...
 	printf("Hello, World!\n");
+	srand(RAND_SEED);
+	for (int i = 0; i < 5; i++) {
+		int r = (rand() %(MAX_HEARTBEAT - MIN_HEARTBEAT)) + MIN_HEARTBEAT;
+		printf("random number =  %d \n", r);
+	}
 	return 0;
 }
 
@@ -31,8 +39,9 @@ void start_child(struct fifo_data *data) {
 	// fork here
     
     // Generate random number for heartbeat rate
+	srand(45);
     int r = (rand() %(MAX_HEARTBEAT - MIN_HEARTBEAT)) + MIN_HEARTBEAT;
-    printf( "random number =  %d \n", r);
+    printf("random number =  %d \n", r);
 }
 
 void end_child() {
