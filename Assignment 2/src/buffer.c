e8a00cc32299081f4b26bd3fc94b9779ce5c8e48
monisha <moni.sind@gmail.com>
2013-11-06 11:41:14 -0500

    added basic code for Shared Memory

--- Assignment 2/src/buffer.c
+++ Assignment 2/src/buffer.c
@@ -7,22 +7,62 @@
 
 #include <stdio.h>
 
-void buffer_init(int size)
-{
-    return;
-}
+int schmid; // shared memory identifier
+int *shared_memory_array; // Pointer to the first byte of shared memory
+
 
 int  buffer_get_size(void)
 {
     return 1;
 }
 
-int  buffer_get_value(int index)
+
+void buffer_init(int buffer_size)
 {
-    return 1;
+    
+    //Create shared memory
+    shmid = shmget((key_t)1234, sizeof(struct shared_use_st), 0666 | IPC_CREAT);
+    
+    if (shmid == -1) {
+        fprintf(stderr, "shmget failed\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    //Make the shared memory accessible
+    shared_memory_array = (int *)shmat(shmid, NULL, 0);
+    
+    if (shared_memory == (void *)-1) {
+        fprintf(stderr, "shmat failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Memory attached at %X\n", (int)shared_memory);
+    return;
 }
 
-void buffer_set_value(int index, int value)
-{
-    return;
+
+int  buffer_get_value(int index){
+    int value = shared_memory_array[index];
+    //********** check: need an indicator that the value has been removed and producer ca add a value to this index.
+    //********** For now I have used 0
+    shared_memory_array[index] = 0;
+    return value;
+}
+
+void buffer_set_value(int index, int value){
+    shared_memory_array[index] = value;
+}
+
+void detach_shared_memory(){
+    if (shmdt(shared_memory) == -1) {
+        fprintf(stderr, "shmdt failed\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    if (shmctl(shmid, IPC_RMID, 0) == -1) {
+        fprintf(stderr, "shmctl(IPC_RMID) failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    exit(EXIT_SUCCESS);
 }
